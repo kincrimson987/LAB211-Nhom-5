e28d0c57860ee9996fd2425d44d8f62e6c23c5f2
nghoainhi45-gif <nghoainhi45@gmail.com>
2026-06-05 23:01:55 +0700

    Update PayrollEntry.java

---
+++ src/model/PayrollEntry.java
@@ -0,0 +1,38 @@
+package model;
+public class PayrollEntry {
+
+    private PayrollStatus status;
+    private int version;
+
+    public PayrollEntry() {
+        this.status = PayrollStatus.PENDING;
+        this.version = 1;
+    }
+
+    public PayrollStatus getStatus() {
+        return this.status;
+    }
+
+    public int getVersion() {
+        return this.version;
+    }
+
+    public void process() {
+
+        if (status == PayrollStatus.PROCESSED) {
+            throw new IllegalStateException(
+                    "Payroll already processed.");
+        }
+
+        this.status = PayrollStatus.PROCESSED;
+        this.version++;
+    }
+
+    @Override
+    public String toString() {
+        return "PayrollEntry{" +
+                "status=" + this.status +
+                ", version=" + this.version +
+                '}';
+    }
+}
