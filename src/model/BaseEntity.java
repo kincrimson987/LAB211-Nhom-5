c3d3d30ea9d28bd6877362cae18c54203a321990
nghoainhi45-gif <nghoainhi45@gmail.com>
2026-06-16 21:02:44 +0700

    Update BaseEntity.java

--- src/model/BaseEntity.java
+++ src/model/BaseEntity.java
@@ -30,12 +30,33 @@ public abstract class BaseEntity {
 
     public abstract void fromCsvLine(String line);
 
+    /*
+     * @Override
+     * public boolean equals(Object o) {
+     * if (this == o) return true;
+     * if (o == null || getClass() != o.getClass()) return false;
+     * BaseEntity that = (BaseEntity) o;
+     * return id != null ? id.equals(that.id) : that.id == null;
+     * }
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+
+        if (o == null) {
+            return false;
+        }
+
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+
         BaseEntity that = (BaseEntity) o;
-        return id != null ? id.equals(that.id) : that.id == null;
+
+        if (id == null || that.id == null) {
+            return false;
+        }
+
+        return id.equals(that.id);
     }
 
     @Override
